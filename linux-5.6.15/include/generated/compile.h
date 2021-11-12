14dca8c056d1ab8f0114634028de4f5ea86d9778
Žiga Jerina <ziga.jerina@gmail.com>
2021-11-12 12:08:47 +0100

    Added linux to project

---
+++ linux-5.6.15/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Fri Nov 5 18:41:26 CET 2021"
+#define LINUX_COMPILE_BY "zjerina"
+#define LINUX_COMPILE_HOST "zjerina-ThinkPad-T470s"
+#define LINUX_COMPILER "gcc version 8.4.0 (Buildroot 2020.05)"
